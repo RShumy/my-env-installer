87e8142c3217bdb900988e12e7a94a2c28890922
RShumy <razvansuhan@yahoo.com>
2025-04-13 11:08:46 +0300

    Added Link command class

---
+++ src/main/java/org/myenv/project/model/commands/link/LinkWindowsEntries.java
@@ -0,0 +1,35 @@
+package org.myenv.project.model.commands.link;
+
+import static org.myenv.project.model.commands.CommandFlag.flag;
+
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.myenv.project.model.commands.CommandAction;
+import org.myenv.project.model.commands.FlagType;
+
+import lombok.Getter;
+
+public enum LinkWindowsEntries {
+
+    FILE(""),
+    DIR("d");
+
+    private CommandAction action;
+
+    @Getter
+    private final Map.Entry<String, CommandAction> entry;
+    
+    LinkWindowsEntries(String option) {
+
+        this.action = StringUtils.isEmpty(option) ? CommandAction.defaultEmptyAction() :
+            CommandAction.defaultEmptyAction()
+            .build(flag(option).withFlagType(FlagType.SLASH));
+        this.entry = Map.entry(option, this.action);
+    }
+
+    public CommandAction get(){
+        return this.action;
+    }
+
+}
