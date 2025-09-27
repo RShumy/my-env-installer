a521067860e9f4c859e5e80ffa54e8108915de0b
RShumy <68147685+RShumy@users.noreply.github.com>
2025-09-27 20:37:39 +0300

    Changed Which class name to AppPath and adapted for a Windows use as well

--- src/main/java/org/myenv/project/model/commands/which/AppPath.java
+++ src/main/java/org/myenv/project/model/commands/which/AppPath.java
@@ -1,6 +1,8 @@
 package org.myenv.project.model.commands.which;
 
+import org.myenv.project.model.OS;
 import org.myenv.project.model.commands.Command;
+import org.myenv.project.utils.os.OSUtil;
 
 import static org.myenv.project.model.commands.CommandAction.emptyAction;
 
@@ -8,17 +10,17 @@ import static org.myenv.project.model.commands.CommandAction.emptyAction;
  * Command to query if an application or program is installed
  * TODO: generify for windows use also, find another namings for Class name and methods
  */
-public class Which extends Command {
+public class AppPath extends Command {
 
-    private Which() {
-        super("which");
+    private AppPath() {
+        super(OSUtil.os.equals(OS.WINDOWS) ? "where" : "which");
     }
 
-    private static Which instance(String argument){
-        return new Which().action(emptyAction().build(argument));
+    private static AppPath instance(String argument){
+        return new AppPath().action(emptyAction().build(argument));
     };
 
     public static String ask(String application) {
-        return Which.instance(application).execute();
+        return AppPath.instance(application).execute();
     }
 }
