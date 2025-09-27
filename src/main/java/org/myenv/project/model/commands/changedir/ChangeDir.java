b43f58a4d0df0a2125cce491405d2e2981eab6a1
RShumy <68147685+RShumy@users.noreply.github.com>
2025-09-28 00:31:29 +0300

    Refined checks for adding the 'd' flag when changeDir is called for windows

--- src/main/java/org/myenv/project/model/commands/changedir/ChangeDir.java
+++ src/main/java/org/myenv/project/model/commands/changedir/ChangeDir.java
@@ -14,13 +14,17 @@ import static org.myenv.project.model.commands.CommandFlag.flag;
 
 public class ChangeDir extends Command {
 
+    private static final String windowsDrivePath = "^[A-Z]:/[A-z0-9=-_ /\\\\(){}\\]\\[&%#@!~`,.]+";
+
     private ChangeDir() {
         super("cd", new HashMap<>());
     }
 
     public static ChangeDir changeDir(String path) {
         OS os = OSUtil.os;
-        if (Objects.requireNonNull(os).equals(WINDOWS) && os.getShell().equals("cmd")) {
+        if (Objects.requireNonNull(os).equals(WINDOWS)
+                && os.getShell().equals("cmd.exe")
+                && path.matches(windowsDrivePath)) {
             return new ChangeDir().action(
                         emptyAction().build(flag("d").withFlagType(FlagType.SLASH).withArgument(path)));
         }
