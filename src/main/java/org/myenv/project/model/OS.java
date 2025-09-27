67f0ab581f7f52c11b5b821802ebd1a61d000b8d
RShumy <68147685+RShumy@users.noreply.github.com>
2025-09-27 20:35:46 +0300

    OS class changes

--- src/main/java/org/myenv/project/model/OS.java
+++ src/main/java/org/myenv/project/model/OS.java
@@ -2,19 +2,19 @@ package org.myenv.project.model;
 
 import lombok.Getter;
 import org.myenv.project.model.commands.packagemanager.PackageManager;
+import org.myenv.project.utils.os.OSUtil;
 
 import static org.myenv.project.utils.os.LinuxInfo.resolveDistroType;
 
 @Getter
 public enum OS {
 
-    WINDOWS("windows", ""),
+    WINDOWS("windows", "windows"),
     LINUX("linux", resolveDistroType()),
-    MACOS("macOS", ""),
-    UNIX("unix", ""),
+    MACOS("macOS", "macOs"),
+    UNIX("unix", "unix"),
     UNKNOWN("unknown", "");
 
-    @Getter
     private final String osTypeAndVersion;
 
     private PackageManager packageManager;
@@ -25,14 +25,15 @@ public enum OS {
 
         this.osTypeAndVersion = osType;
 
-        switch (name.toUpperCase()) {
+        switch (name) {
             /* TODO ? method for the user to choose the preferred Windows shell (cmd or powershell) */
-            case "WINDOWS" ->
-                this.withShell("cmd")
+            case OSUtil.WINDOWS ->
+                this.withShell("cmd.exe")
                     .withPackageManager(new PackageManager(this));
-            case "LINUX","UNIX" ->
-                this.withShell("bash").withPackageManager(new PackageManager(this));
-            case "MACOS" ->
+            case OSUtil.LINUX, OSUtil.UNIX ->
+                this.withShell("bash")
+                        .withPackageManager(new PackageManager(this));
+            case OSUtil.MACOS ->
                 this.withShell("zsh")
                     .withPackageManager(new PackageManager(this));
             default ->
@@ -51,6 +52,18 @@ public enum OS {
 
     public String getName(){
         return this.name().toLowerCase();
+    }
+
+    public boolean isWindows() {
+        return this.equals(OS.WINDOWS);
+    }
+
+    public boolean isMacOS() {
+        return this.equals(OS.MACOS);
+    }
+
+    public boolean isLinux() {
+        return this.equals(OS.LINUX);
     }
 
 }
