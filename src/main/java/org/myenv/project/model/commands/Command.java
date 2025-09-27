2327ac6c93e48c40c96d2aa7d1b547e60041c1f1
RShumy <68147685+RShumy@users.noreply.github.com>
2025-09-27 20:40:51 +0300

    Changed Command to run for Windows as well

--- src/main/java/org/myenv/project/model/commands/Command.java
+++ src/main/java/org/myenv/project/model/commands/Command.java
@@ -1,9 +1,14 @@
 package org.myenv.project.model.commands;
 
+import org.myenv.project.model.OS;
+import org.myenv.project.utils.os.OSUtil;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.myenv.project.utils.os.OSUtil.*;
 
 
 public abstract class Command {
@@ -23,7 +28,7 @@ public abstract class Command {
     }
 
     public <T extends Command> T action(CommandAction action) {
-        this.finalCommand = String.join(" ",mainCommand , action.getFinalAction().trim());
+        this.finalCommand = String.join(" ", mainCommand, action.getFinalAction().trim());
         return (T) this;
     }
 
@@ -32,7 +37,8 @@ public abstract class Command {
         int exitCode = 0;
         try {
             // Start the process (Example: Running 'bash' or 'cmd' for interactive mode)
-            ProcessBuilder builder = new ProcessBuilder("bash", "-c", finalCommand);
+            System.out.println("Running command: " + finalCommand);
+            ProcessBuilder builder = new ProcessBuilder(os.getShell(), getShellCommandFlag(), finalCommand);
             builder.redirectErrorStream(true);
 
             Process process = builder.start();
@@ -44,8 +50,9 @@ public abstract class Command {
             String line;
 
             while ((line = reader.readLine()) != null) {
-               output.append(line);
-                System.out.println(line);// Print process output
+                output.append(line);
+                // Print process output
+                System.out.println(line);
             }
 
             // Wait for process to complete
@@ -57,13 +64,16 @@ public abstract class Command {
 
         if (exitCode == 1)
             return "ERROR while running Command: " + finalCommand;
-        else return output.toString();
+        else
+            return output.toString();
     }
 
     public String get() {
         return this.finalCommand;
     }
 
-
+    private String getShellCommandFlag() {
+       return os.isWindows() ? "/c" : "-c";
+    }
 
 }
